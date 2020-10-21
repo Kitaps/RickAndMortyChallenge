a9d7875669e64a3248f7515a946f86fe4cb2d30d
Kitaps <paliedke@uc.cl>
2020-10-21 03:04:07 -0300

    add location finder
    add method to find the origin locations of characters from a episode

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import numpy as np
+from time import time
+
 from api_collector import api_get
 from counter import letter_appearance
+from char_finder import chars_from, origins_from
 
 # Part 1
 
@@ -21,7 +25,14 @@ episodes_count = letter_appearance("e", episodes)
 # Display results
 print(f"The letter \"l\" appears {locations_count} in location names.")
 print(f"The letter \"c\" appears {characters_count} in character names.")
-print(f"The letter \"e\" appears {episodes_count} in episode names.")
+print(f"The letter \"e\" appears {episodes_count} in episode names.\n")
 
 # Part 2
+
+# Make numpy array from characters list to make faster operations
+characters_array = np.array(characters)
+aux_chars = chars_from(episodes[0], characters_array)
+# print(aux_chars)
+print(origins_from(aux_chars))
+# print(episodes[0]["characters"])
 
