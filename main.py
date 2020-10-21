ed51d9afe7407366cd8086b91196dff96cc2a448
Kitaps <paliedke@uc.cl>
2020-10-21 14:51:22 -0300

    done
    correctly display origin locations of characters who appear on each episode

--- main.py
+++ main.py
@@ -31,8 +31,23 @@ print(f"The letter \"e\" appears {episodes_count} in episode names.\n")
 
 # Make numpy array from characters list to make faster operations
 characters_array = np.array(characters)
-aux_chars = chars_from(episodes[0], characters_array)
-# print(aux_chars)
-print(origins_from(aux_chars))
-# print(episodes[0]["characters"])
+
+# Get characters for each episode applying chars_from on each episode of episodes list
+episodes_characters = map(lambda episode: chars_from(episode, characters_array), episodes)
+
+# Get locations sets for each episode applying origins_from on each character set
+# These caracter sets were obtained on the previous operation. 
+episodes_locations = map(lambda episode_chars: origins_from(episode_chars), episodes_characters)
+
+# Turn the obtained sets into tuples like: (set_lenght, set)
+# To obtain how many locations appear on each episode, and their names
+episodes_locations_tuples = list(map(lambda episode_locations: (len(episode_locations), episode_locations), episodes_locations))
+
+# Display results
+episode_id = 1
+for locs in episodes_locations_tuples:
+    print(f"{locs[0]} lugares en el episodio {episode_id}: {locs[1]}")
+    print()
+    episode_id += 1
+
 
