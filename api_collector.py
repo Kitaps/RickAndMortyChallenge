768913c9ade103deda5900fe9af94fdb5ea2f375
Kitaps <paliedke@uc.cl>
2020-10-21 19:21:34 -0300

    update api_get
    now working on threads

--- api_collector.py
+++ api_collector.py
@@ -1,37 +1,102 @@
 # Module to get the API data and preprocess it
 import requests
+import numpy as np
+from threading import Thread
 
 def api_get(category):
     # Recives a category string: "location"; "character"; "episode"
     # Acess API and with GET query
-    get_data = requests.get(f"https://rickandmortyapi.com/api/{category}/")
-    
-    results = []
+    # We get the first page and information of how many pages to expect
+    link = f"https://rickandmortyapi.com/api/{category}/"
+    first_data = requests.get(link)
 
-    # Iterate untill all pages have been requested
-    while get_data.status_code == 200: 
-        # If request was successfull, parse as json
-        get_data = get_data.json()
-        # Save result and next page
-        results += get_data["results"]
-        next_page = get_data["info"]["next"]
-
-        if next_page:
-            # We get the next page from the request
-            get_data = requests.get(next_page)
-        # If all pages have been visited (next == null) return 
-        else: 
-            return results
-    
-    else: 
-        # If not, return empty result, notify by print
-        print(f"\nCould not access the API to get {category}...\n")
+    # Make sure the request was made properly
+    if first_data.status_code != 200:
+        print(f"Unsuccessful request: {first_data.status_code}")
         return []
+
+    # We want it parsed as JSON
+    first_data = first_data.json()
+
+    # We get the number of pages for the category to get
+    pages = int(first_data["info"]["pages"])
+
+    # We initialize an array to fill with the respective page results
+    page_results = np.empty(pages, dtype=list)
+
+    # We save the first page results, which have already been requested
+    page_results[0] = first_data["results"]
+
+    # Now the fun starts, we will make multiple threads, one for each page. 
+    # These threads will save their results in their respective page_result slot. 
+    threads = []
+    for i in range(1, pages):
+        # For each page, we make a thread to get the page data
+        t = Thread(target=get_page, args=(link, i, page_results))
+        # We start the thread and save it to be accesible
+        t.start()
+        threads.append(t)
+    
+    # We now make that the function waits for all threads to finish before continuing
+    for t in threads:
+        t.join()
+
+    # Once all data has been get, we join the pages data into one
+    results = np.concatenate(page_results)
+
+    return results
+
+    
+
+    # # Iterate untill all pages have been requested
+    # while get_data.status_code == 200: 
+    #     # If request was successfull, parse as json
+    #     get_data = get_data.json()
+    #     # Save result and next page
+    #     results += get_data["results"]
+    #     next_page = get_data["info"]["next"]
+
+    #     if next_page:
+    #         # We get the next page from the request
+    #         get_data = requests.get(next_page)
+    #     # If all pages have been visited (next == null) return 
+    #     else: 
+    #         return results
+    
+    # else: 
+    #     # If not, return empty result, notify by print
+    #     print(f"\nCould not access the API to get {category}...\n")
+    #     return []
+
+def get_page(link, index, destiny):
+    # Recives the category link, the page_results index of where to save the data
+    # and the desteny of where to save (page_results)
+
+    # Pages are always one more than the index
+    # For example the first page has id 1, but is saved in page_results[0]
+    page_id = index + 1
+    # We take the category link and the index and concatenate it into the page link.
+    page_link = "".join([link, "?page=", str(page_id)])
+
+    page_data = requests.get(page_link).json()
+
+    # print(f"Dentro de results {index} hay: {destiny[index]}")
+
+    # We save the get data in the corresponding memory space
+    destiny[index] = page_data["results"]
+
+    # print(f"Dentro de results {index} hay: {destiny[index]}")
 
 if __name__ == "__main__":
     pass
-    import json
-    # print(json.dumps(api_get("location"), indent=4))
-    # print(json.dumps(api_get("character"), indent=4))
-    # print(json.dumps(api_get("episode"), indent=4))
+    
+    # Parameters are: "location" ; "character" ; "episode"
+    
+    # data = api_get("episode")
+    # print(data[0])
+    # print()
+    # print(data[1])
+    # print()
+    # print(data[-1])
+    
 
